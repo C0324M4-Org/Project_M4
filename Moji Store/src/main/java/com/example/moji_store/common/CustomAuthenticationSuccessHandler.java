999e98467cad74f8ceec72e2d2a2338876bf9f22
itachialy <zassuo@gmail.com>
2024-11-11 22:35:00 +0700

    clear

--- Moji Store/src/main/java/com/example/moji_store/common/CustomAuthenticationSuccessHandler.java
+++ Moji Store/src/main/java/com/example/moji_store/common/CustomAuthenticationSuccessHandler.java
@@ -15,14 +15,10 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        // Lấy URL từ session attribute tự định nghĩa (nếu có)
         String targetUrl = (String) request.getSession().getAttribute("REDIRECT_URL");
 
-        // Kiểm tra nếu không có REDIRECT_URL trong session, chuyển hướng dựa trên vai trò
         if (targetUrl == null || targetUrl.isEmpty()) {
-            // Lấy tên người dùng đang đăng nhập
             String username = authentication.getName();
-            // Kiểm tra vai trò của người dùng
             boolean isAdmin = authentication.getAuthorities().stream()
                     .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
 
@@ -33,10 +29,8 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             }
         }
 
-        // Xóa attribute khỏi session sau khi sử dụng
         request.getSession().removeAttribute("REDIRECT_URL");
 
-        // Tiến hành chuyển hướng người dùng đến URL đã xác định
         clearAuthenticationAttributes(request);
         getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
