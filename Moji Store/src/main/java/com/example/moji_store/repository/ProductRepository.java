81940bb5bc1b9a4fc67d8f29b7356193f94e9635
KhoaITdn <166222072+KhoaITdn@users.noreply.github.com>
2024-11-21 13:00:05 +0700

    Delete Moji Store directory

--- Moji Store/src/main/java/com/example/moji_store/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.moji_store.repository;
-
-import com.example.moji_store.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
