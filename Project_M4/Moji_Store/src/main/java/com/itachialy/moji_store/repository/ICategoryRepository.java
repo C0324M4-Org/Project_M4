4c7b0989f9acf2760f4c6b0fd1db421a4c616643
Le Quang Viet <166206730+itachialy@users.noreply.github.com>
2024-11-21 13:53:11 +0700

    Delete Project_M4/Moji_Store directory

--- Project_M4/Moji_Store/src/main/java/com/itachialy/moji_store/repository/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.itachialy.moji_store.repository;
-
-import com.itachialy.moji_store.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICategoryRepository extends JpaRepository<Category,Long> {
-}
