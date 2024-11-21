9fefe209366cee59f9014516fa3fd7809cf3789e
KhoaITdn <khoabkdn2003@gmail.com>
2024-11-21 13:05:33 +0700

    Add Moji_Store and remove unwanted files

--- Moji Store/src/main/java/com/example/moji_store/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.moji_store.repository;
-
-import com.example.moji_store.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    Cart findByUserId(String userId); // Tìm giỏ hàng theo người dùng
-}
