aedf6f1b83088420568ea28d8d5ef41e6e6e3b7a
Le Quang Viet <166206730+itachialy@users.noreply.github.com>
2024-11-26 20:10:47 +0700

    Delete Project_M4 directory

--- Project_M4/Moji_Store/src/main/java/com/itachialy/moji_store/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.itachialy.moji_store.repository;
-
-import com.itachialy.moji_store.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Account findByUsername(String username); // Custom method to find an account by username
-}
--- Project_M4/Moji_Store/src/main/java/com/itachialy/moji_store/repository/IImageRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.itachialy.moji_store.repository;
-//
-//import com.itachialy.moji_store.model.Image;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface IImageRepository extends JpaRepository<Image, Long> {
-//
-//
-//}
